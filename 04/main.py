578578466f2d648d3ddb850886fbe7d44b16ac05
Jasmine English <contact@jasmineenglish.com>
2018-12-07 00:53:58 -0500

    sorted log by datetime, made dict of guards and their sleep/wake times. next: find guard who sleeps the most.

--- 04/main.py
+++ 04/main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 # year-month-day hour:minute
 
@@ -10,21 +10,47 @@ import datetime
 def get_log(input='input.txt'):
     with open(input, 'r') as file:
         # I don't think the hard slices are an elegant solution
-        log = {}
+        log = []
         for line in list(file):
-            k = line[1:17]
+            k = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')  # time
             if '#' in line:
-                v = line[27:].split(' ')[0]
+                # guard id if there, otherwise, log message
+                v = int(line[27:].split(' ')[0])
             else:
                 v = line[19:-1]
-            log[k] = v
+            log.append((k, v))
         file.close
 
     return log
 
 
-# def sort_log():
+def get_sorted_log(log=get_log()):  # sort by date
+    return sorted(log, key=lambda entry: entry[0])
 
-# this is where the 'slice not hashable' issue was happening.
-# try list comprehension ^^^
-print(get_log())
+
+def get_guards_sleeptime(log=get_sorted_log()):
+    guards = {}
+    for entry in log:
+        if isinstance(entry[1], int):
+            id = entry[1]
+            last_guard = id
+            if id in guards:
+                guards[id].append((entry[0], True))
+            else:
+                guards[id] = [(entry[0], True)]
+        elif entry[1] == 'wakes up':
+            guards[last_guard].append((entry[0], True))
+        else:
+            guards[last_guard].append((entry[0], False))
+
+    return guards
+
+# this is where the 'slice not hashable' issue was happening,
+# because I was trying to slice the dict get_log returned (you can't slice dicts).
+# try list comprehension in get_log instead of for loop
+# print(get_log())
+
+# print(sort_log())
+
+
+print(get_guards_sleeptime())
