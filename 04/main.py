e0fe4b502ba56a59e7fc0a7a481e27f0e41de4ff
Jasmine English <contact@jasmineenglish.com>
2018-12-07 02:11:26 -0500

    I think I got sleepiest guard. Next: which minute does he sleep the most on?

--- 04/main.py
+++ 04/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from collections import Counter
 
 # year-month-day hour:minute
 
@@ -35,22 +36,35 @@ def get_guards_sleeptime(log=get_sorted_log()):
             id = entry[1]
             last_guard = id
             if id in guards:
-                guards[id].append((entry[0], True))
+                guards[id].append((entry[0], 0))
             else:
-                guards[id] = [(entry[0], True)]
+                guards[id] = [(entry[0], 0)]
         elif entry[1] == 'wakes up':
-            guards[last_guard].append((entry[0], True))
+            guards[last_guard].append((entry[0], 0))
         else:
-            guards[last_guard].append((entry[0], False))
+            guards[last_guard].append((entry[0], 1))
 
     return guards
 
-# this is where the 'slice not hashable' issue was happening,
-# because I was trying to slice the dict get_log returned (you can't slice dicts).
-# try list comprehension in get_log instead of for loop
-# print(get_log())
 
-# print(sort_log())
+def get_sleepiest_guard(guards=get_guards_sleeptime()):  # 963
+    sleep_times = {}
+    # count awake times in each guard's log entry list
+    for guard in guards.items():
+        time_asleep = sum(entry[1] for entry in guard[1])
+        sleep_times[guard[0]] = time_asleep
+
+    return max(sleep_times)
 
 
-print(get_guards_sleeptime())
+if __name__ == "__main__":
+    # this is where the 'slice not hashable' issue was happening,
+    # because I was trying to slice the dict get_log returned (you can't slice dicts).
+    # try list comprehension in get_log instead of for loop
+    # print(get_log())
+
+    # print(sort_log())
+
+    # print(get_guards_sleeptime().values())
+
+    print(get_sleepiest_guard())
